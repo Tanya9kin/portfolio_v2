875ac4f35484a1ba8aeb63f12f861fe09acdffa1
Tanya D <tanya9kin@gmail.com>
2023-06-12 17:12:02 +0300

    removed .dist folder

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import Education from "./components/Education/Education";
-import Header from "./components/Header/Header";
-import Misc from "./components/Misc/Misc";
-import Navigation from "./components/Navigation.js";
-import Projects from "./components/Projects.js/Projects";
-import Skills from "./components/Skills/Skills";
 import { Container } from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
-
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+import Header from "./components/Header/Header";
+import Projects from "./components/Projects.js/Projects";
+import Navigation from "./components/Navigation.js";
+import Education from "./components/Education/Education";
+import Skills from "./components/Skills/Skills";
+import Misc from "./components/Misc/Misc";
 
 const theme = createTheme({
   palette: {
